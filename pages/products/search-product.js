6b289cf7215b6ebdf2fdf127bb70bd6ac7173936
Stilone <54247765+Stilone@users.noreply.github.com>
2021-04-05 05:42:44 +0300

    First commit

--- pages/products/search-product.js
+++ pages/products/search-product.js
@@ -1,5 +1,6 @@
 const search = document.getElementById('search');
 const searchBtn = document.getElementById('search-btn');
+const searchBtnUpdate = document.getElementById('search-btn-update');
 
 
 
@@ -13,4 +14,8 @@ searchBtn.addEventListener('click', () => {
     const searchProducts = mockProducts.filter(productsCheck);
     console.log(searchProducts);
     renderProducts(searchProducts);
+})
+
+searchBtnUpdate.addEventListener('click', () => {
+    renderProducts(mockProducts);
 })
