b2dec5f9e2ea09e700de1d4bbebc879dabd4aeec
Stilone <54247765+Stilone@users.noreply.github.com>
2021-04-08 13:23:42 +0300

    local-storege
    123

---
+++ pages/products/local-storage.js
@@ -0,0 +1,47 @@
+
+// localStorage.one = 2;
+// localStorage.qwerty = 'Dima';
+// localStorage.array = ['nina', 'vlada', 'sasha', 6];
+// console.log(localStorage.one);
+//
+//
+// sessionStorage.thre = JSON.stringify([1,2,3,4])
+// let thre = JSON.parse(sessionStorage.thre);
+// console.log(thre[1]);
+// console.log(JSON.stringify(localStorage, null, 2));
+// console.log(JSON.stringify(localStorage, null, ));
+// console.log(JSON.stringify(localStorage.array));
+//
+// area.value = localStorage.getItem('area');//получить данные по ключу
+// area.oninput = () => {
+//     localStorage.setItem('area', area.value)//сохранить пару ключ, значение
+// };
+
+
+
+// let up = ['nina','vlada','sasha',6];
+// localStorage.second = up;
+// //let down = JSON.stringify(localStorage.second, null,2);
+// let down = JSON.stringify(up, null,2);
+// console.log(up);
+// console.log(localStorage.second);
+// console.log(down);
+// console.log(JSON.parse(down));
+
+let array1 = {
+    name: 'dima',
+    age: 25,
+    date: '29.09.1995',
+}
+
+result = JSON.stringify(array1);
+result2 = JSON.parse(result)
+
+console.log(result);
+console.log(result2);
+
+let array2 = `{
+    "name":"dima",
+`
+let result3 = JSON.parse(array2);
+console.log(result3);
