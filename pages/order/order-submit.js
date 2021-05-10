d8684dc456dc355dbf22b600b4d03953d14dbd09
Stilone <54247765+Stilone@users.noreply.github.com>
2021-05-10 16:07:50 +0300

    123
    123

--- pages/order/order-submit.js
+++ pages/order/order-submit.js
@@ -1,63 +1,38 @@
 const form = document.getElementById('form');
-const regExpName = /^[a-z0-9_-]{3,16}$/;
-const regExpEmail = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
-const regExpPhone = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
-let isValidate = false;
+// const regExpName = /^[a-z0-9_-]{3,16}$/;
+// const regExpEmail = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
 
 
-const submit = () => {
-    alert('данные отправлены!');
-}
-
-const validateElem = (elem) => {
-    if (elem.name === 'name'){
-        if(!regExpName.test(elem.value) && elem.value !== '') {
-            elem.nextElementSibling.textContent = 'Введите корректное имя пользователя';
-            isValidate = false;
-        }else {
-            elem.nextElementSibling.textContent = '';
-            isValidate = true;
-        }
-    }
-    if (elem.name === 'email') {
-        if(!regExpEmail.test(elem.value) && elem.value !== '') {
-            elem.nextElementSibling.textContent = 'Введите корректный Email';
-            isValidate = false;
-        }else {
-            elem.nextElementSibling.textContent = '';
-            isValidate = true;
-        }
-    }
-    if (elem.name === 'phone') {
-        if(!regExpPhone.test(elem.value) && elem.value !== '') {
-            elem.nextElementSibling.textContent = 'Введите корректный номер';
-            isValidate = false;
-        }else {
-            elem.nextElementSibling.textContent = '';
-            isValidate = true;
-        }
-    }
-}
-for(let elem of form.elements) {
-    elem.addEventListener('blur', () => {
-        validateElem(elem);
-    });
-}
 
 form.addEventListener('submit', (even) => {
     even.preventDefault();
 
-    for(let elem of form.elements) {
-        console.log(form.elements[0]);
-        if (elem.value === '') {
-            elem.nextElementSibling.textContent = 'данное поле не заполненно!';
-            isValidate = false;
-        }else {
-            isValidate = true;
-        }
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+
+    const validatePhone = (phone) => {
+        const regExpPhone = /^([+]{1}[7]{1}[0-9]{3}[0-9]{3}[0-9]{2}[0-9]{2})$/;
+        return regExpPhone.test(phone);
     }
-    if(isValidate) {
-        submit();
-        form.reset();
+
+    if(!validatePhone(phone)) {
+        alert('Введите номер с +7');
+    }else {
+        alert('Данные отправлены!')
     }
-})
+
+     function Submit(name, email, phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    console.log(new Submit(name, email, phone))
+
+
+});
+
+
+//при отправки формы собрать все данные из input в объект и вывести его в консоль. 3ч.
+//с помощью js проверять есть ли в номере телефона +7 4ч.
